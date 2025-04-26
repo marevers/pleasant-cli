9a9cd8b6d2835504d04143555ffab446c3f21019
Martijn Evers <mevers@gk-software.com>
2025-04-26 11:07:24 +0200

    Bump version to v0.9.0

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,7 @@ var cfgFile string
 var tokenFile string
 
 // Pleasant-CLI version
-var version = "v0.8.5"
+var version = "v0.9.0"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
